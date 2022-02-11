423e874b4227422840652343b4f98504597e8c9a
bamblebam <burhanizrangwala@gmail.com>
2022-02-11 19:54:03 +0530

    hardhat and nextJS

---
+++ nftapis/pages/intersection.js
@@ -0,0 +1,10 @@
+import { Fragment } from "react";
+import { Moralis } from "moralis";
+
+export default function Intersection() {
+  return (
+    <Fragment>
+      <h1>Intersection</h1>
+    </Fragment>
+  );
+}
