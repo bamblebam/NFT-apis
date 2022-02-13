df6b4c3c3f63b0a72127ba24169caa6dfe2f92cb
bamblebam <burhanizrangwala@gmail.com>
2022-02-13 13:17:02 +0530

    nft data from etherscan

--- nftapis/pages/listing.js
+++ nftapis/pages/listing.js
@@ -1,6 +1,7 @@
 import { Fragment } from "react";
 import { useState } from "react";
 import { Moralis } from "moralis";
+const axios = require("axios");
 // const sdk = require("api")("@opensea/v1.0#1j3wv35kyd6wqwc");
 
 export default function Listing() {
@@ -9,8 +10,12 @@ export default function Listing() {
 
   async function getEvents() {
     const owner = "0x213178de4cc876a18a9cb6f3ed6ea71d0856601a";
-    const option = { address: owner };
-    const nfts = await Moralis.Web3API.account.getNFTs(option);
+    // const option = { address: owner };
+    // const nfts = await Moralis.Web3API.account.getNFTs(option);
+    const nfts = await axios.get(
+      `https://api.etherscan.io/api?module=account&action=tokennfttx&address=${owner}&page=1&offset=100&startblock=0&endblock=27025780&sort=asc&apikey=YourApiKeyToken`
+    );
+    console.log("nfts");
     console.log(nfts);
   }
   return (
